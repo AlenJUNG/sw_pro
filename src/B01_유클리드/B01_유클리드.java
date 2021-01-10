8a69b65f4bff2fa73de667e3ccb46aa92ec1c71e
shadyhj <42958372+shadyhj@users.noreply.github.com>
2021-01-10 11:11:13 +0900

    test

--- src/B01_유클리드/B01_유클리드.java
+++ src/B01_유클리드/B01_유클리드.java
@@ -3,8 +3,8 @@ package B01_유클리드;
 public class B01_유클리드 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stubddddddddd
+s
 	}
 
 }
