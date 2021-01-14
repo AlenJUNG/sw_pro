8ce37dc3e998ebc1ea271e530645622070852900
shadyhj <42958372+shadyhj@users.noreply.github.com>
2021-01-14 21:55:11 +0900

    Update sw_pro_time : /100

--- src/O01_getRuntime/O01_getRuntime.java
+++ src/O01_getRuntime/O01_getRuntime.java
@@ -21,6 +21,10 @@ public class O01_getRuntime {
 		
 		long minDiffTime = secDiffTime/60;
 		System.out.println("시간차이(m) : "+minDiffTime);
+		
+		// 시험에서 측정은 아래와 같이 한다 ex) 15 > 1.5초
+		long sw_pro_time = (afterTime - beforeTime) / 100;
+		System.out.println("시간차이(pro_time) : " + sw_pro_time);
 
 	}
 
