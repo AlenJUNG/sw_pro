52934edaf0e9ebcd37c7109f2de4f940066c5b11
shadyhj <42958372+shadyhj@users.noreply.github.com>
2021-01-18 22:38:55 +0900

    Add D04_인덱스트리

---
+++ src/D04_인덱스트리/D04_인덱스트리.java
@@ -0,0 +1,15 @@
+package D04_¿Œµ¶Ω∫∆Æ∏Æ;
+
+import java.util.*;
+import java.io.*;
+
+public class D04_¿Œµ¶Ω∫∆Æ∏Æ {
+
+	public static void main(String[] args) throws IOException{
+		System.setIn(new FileInputStream("src/D04_¿Œµ¶Ω∫∆Æ∏Æ/D04_¿Œµ¶Ω∫∆Æ∏Æ.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+	}
+
+}
