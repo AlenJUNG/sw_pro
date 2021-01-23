1c30f2daf44bc47e218cd6faccf8a871676e014f
shadyhj <shadyhj.@gmail.com>
2021-01-23 16:37:10 +0900

    Update G06_ex03_Adv_100_1761_정점들의거리

--- src/G06_LCA/G06_ex03_Adv_100_1761_정점들의거리.java
+++ src/G06_LCA/G06_ex03_Adv_100_1761_정점들의거리.java
@@ -1,136 +1,124 @@
 package G06_LCA;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
+
+// G06_ex03_Adv_100_1761_정점들의거리 Update 중 (2021.01.23)
 
 public class G06_ex03_Adv_100_1761_정점들의거리 {
+	static int MAX_N = 40000;
+	static int MAX_D = 16;
 	static int N, M;
-	static int parent[], depth[], distance[];
-	static ArrayList<Integer> wire[];
-	static ArrayList<Integer> distance_wire[];
-	
-	@SuppressWarnings("unchecked")
-	public static void main(String[] args) throws IOException{
+	static int parent[][], depth[], distance[];
+	static ArrayList<ArrayList<Integer>> wire, distance_wire;
+
+	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/G06_LCA/G06_ex03_Adv_100_1761_정점들의거리.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		StringTokenizer st;
+
 		N = Integer.parseInt(br.readLine());
-		parent = new int[N + 1];
 		depth = new int[N + 1];
 		distance = new int[N + 1];
-		wire = new ArrayList[N + 1];
-		distance_wire = new ArrayList[N + 1];
-		for(int i = 1; i <= N; i++) {
-			wire[i] = new ArrayList<Integer>();
-			distance_wire[i] = new ArrayList<Integer>();
-		}
-		
-		for(int i = 1; i <= N - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			wire[a].add(b);
-			wire[b].add(a);
-			
-			distance_wire[a].add(c);	// 마지막에 거리 추가
-			distance_wire[b].add(c);	// 마지막에 거리 추가
-		}
-		
-		BFS(1, 0, 0);
-		
-//		System.out.println(distance[7]);
-		
-		M = Integer.parseInt(br.readLine());
-		
-		for(int i = 1; i <= M; i++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st1.nextToken());
-			int b = Integer.parseInt(st1.nextToken());
-			
-			int lca = LCA(a, b);
-			int ans = distance[a] + distance[b] - 2*distance[lca];
-			//a ~ b까지의 거리 = a까지 거리 + b까지의 거리 - 2 * (LCA까지의 거리)
-			
-			bw.write(ans + "\n");
-			
-		}
-		
-		br.close();
-		bw.flush();
-		bw.close();
+		parent = new int[MAX_D + 1][N + 1];
 
+		wire = new ArrayList<ArrayList<Integer>>();
+		distance_wire = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i <= N; i++) {
+			wire.add(new ArrayList<Integer>());
+			distance_wire.add(new ArrayList<Integer>());
+		}
+
+		int from, to, cost;
+		for (int i = 1; i <= N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			from = Integer.parseInt(st.nextToken());
+			to = Integer.parseInt(st.nextToken());
+			cost = Integer.parseInt(st.nextToken());
+
+			wire.get(from).add(to);
+			wire.get(to).add(from);
+
+			distance_wire.get(from).add(cost);
+			distance_wire.get(to).add(cost);
+		}
+
+		BFS(1);
+
+		M = Integer.parseInt(br.readLine());
+
+		int a, b;
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+
+			int x = distance[LCA(a, b)];
+			//a ~ b까지의 거리 = a까지 거리 + b까지의 거리 - 2 * (LCA까지의 거리)
+			System.out.println(distance[a] + distance[b] - 2 * x);			
+		}
+
+		br.close();
+	}
+
+	private static void BFS(int root) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(root);
+		depth[root] = 0;
+		parent[0][root] = 0;
+		distance[root] = 0;
+
+		while (!q.isEmpty()) {
+			int now = q.poll();
+
+			for (int toIdx = 0; toIdx < wire.get(now).size(); toIdx++) {
+				int next = wire.get(now).get(toIdx);
+
+				if (next == parent[0][now]) {
+					continue;
+				}
+
+				q.offer(next);
+				depth[next] = depth[now] + 1;
+				parent[0][next] = now;
+
+				distance[next] = distance[now] + distance_wire.get(now).get(toIdx);
+
+				for (int i = 1; i <= MAX_D; i++) {
+					if (parent[i - 1][next] == 0) {
+						break;
+					}
+					parent[i][next] = parent[i - 1][parent[i - 1][next]];
+				}
+			}
+		}
 	}
 
 	private static int LCA(int a, int b) {
-		while(depth[a] != depth[b]) {
-			if(depth[a] > depth[b]) {
-				a = parent[a];				
-			}else {
-				b = parent[b];
+		if (depth[a] > depth[b]) {
+			int temp;
+			temp = a;
+			a = b;
+			b = temp;
+		}
+
+		for (int i = MAX_D; i >= 0; i--) {
+			if (depth[b] - depth[a] >= Math.pow(2, i)) {
+				b = parent[i][b];
 			}
 		}
-		
-		if(a == b) {
+
+		if (a == b) {
 			return a;
 		}
-		
-		while(a != b) {
-			a = parent[a];
-			b = parent[b];
-		}
-		
-		return a;
-	}
 
-	private static void BFS(int root, int dep, int dis) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(root);
-		parent[root] = 0;
-		depth[root] = 0;
-		distance[root] = 0;
-		
-		while(!q.isEmpty()) {
-			int v = q.poll();
-			
-			for(int i = 0; i < wire[v].size(); i++) {
-				int next = wire[v].get(i);
-				if(next == parent[v]) {
-					continue;
-				}				
-				q.offer(next);
-				parent[next] = v;
-				depth[next] = depth[v] + 1;
-				distance[next] = distance[v] + distance_wire[v].get(i);
-			}			
+		for (int i = MAX_D; i >= 0; i--) {
+			if (parent[i][b] != parent[i][a]) {
+				a = parent[i][a];
+				b = parent[i][b];
+			}
 		}
-		
-	}
 
+		return parent[0][a];
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
