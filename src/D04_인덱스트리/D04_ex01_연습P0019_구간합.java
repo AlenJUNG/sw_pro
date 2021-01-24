231a21010713622b04e72854131ff45c5a9afcab
shadyhj <shadyhj.@gmail.com>
2021-01-25 06:59:26 +0900

    Update

--- src/D04_인덱스트리/D04_ex01_연습P0019_구간합.java
+++ src/D04_인덱스트리/D04_ex01_연습P0019_구간합.java
@@ -3,7 +3,7 @@ package D04_인덱스트리;
 import java.util.*;
 import java.io.*;
 
-public class D04_인덱스트리 {
+public class D04_ex01_연습P0019_구간합 {
 
 	public static void main(String[] args) throws IOException{
 		System.setIn(new FileInputStream("src/D04_인덱스트리/D04_인덱스트리.txt"));
