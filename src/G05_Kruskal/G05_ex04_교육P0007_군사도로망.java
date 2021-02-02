26e1583ddce3c97e32e63190a1d6501e21bf2217
shadyhj <shadyhj.@gmail.com>
2021-02-02 19:41:03 +0900

    Add

---
+++ src/G05_Kruskal/G05_ex04_교육P0007_군사도로망.java
@@ -0,0 +1,116 @@
+package G05_Kruskal;
+
+import java.util.*;
+import java.io.*;
+
+// Tip : 문제를 넓게 보자
+public class G05_ex04_교육P0007_군사도로망 {
+	static class Node implements Comparable<Node> {
+		int from, to, cost;
+
+		public Node(int from, int to, int cost) {
+			this.from = from;
+			this.to = to;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node other) {
+			if (this.cost < other.cost) {
+				return -1;
+			}
+			return 1;
+		}
+	}
+
+	static int TC, N, M, K;
+	static int parent[];
+	static long ans;
+	static PriorityQueue<Node> pq;
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("src/G05_Kruskal/G05_ex04_교육P0007_ 군사도로망.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+//		bw.write(Math.pow(10, 6) + "\n");
+
+		TC = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= TC; tc++) {
+
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+
+			parent = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				parent[i] = i;
+			}
+
+			pq = new PriorityQueue<>();
+
+			int a, b, x;
+			for (int i = 1; i <= M; i++) {
+				st = new StringTokenizer(br.readLine());
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
+				x = Integer.parseInt(st.nextToken());
+
+				pq.offer(new Node(a, b, -x));
+				ans += x; // 모든 도로 제거 시 총합 > key point
+			}
+
+			for (int i = 1; i <= K; i++) {
+				st = new StringTokenizer(br.readLine());
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
+				x = Integer.parseInt(st.nextToken());
+
+				pq.offer(new Node(a, b, x));
+
+			}
+
+			while (!pq.isEmpty()) {
+				Node node = pq.poll();
+
+				if (find(node.to) == find(node.from)) {
+					continue;
+				} else {
+					union(node.to, node.from);
+					ans += node.cost;
+				}
+
+			}
+
+			bw.write("#" + tc + " " + ans + "\n");
+
+		}
+
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+
+		if (a > b) {
+			parent[a] = parent[b];
+		} else {
+			parent[b] = parent[a];
+		}
+
+	}
+
+	private static int find(int x) {
+		if (x == parent[x]) {
+			return x;
+		}
+		return parent[x] = find(parent[x]);
+	}
+
+}
