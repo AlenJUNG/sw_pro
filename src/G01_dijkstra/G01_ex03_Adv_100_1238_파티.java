cf451f1a7d061d3cb1e4fe3d9efb528d936c3220
shadyhj <shadyhj.@gmail.com>
2021-05-19 10:34:45 +0900

    Update

--- src/G01_dijkstra/G01_ex03_Adv_100_1238_파티.java
+++ src/G01_dijkstra/G01_ex03_Adv_100_1238_파티.java
@@ -7,104 +7,129 @@ import java.util.*;
 import java.io.*;
 
 public class G01_ex03_Adv_100_1238_파티 {
-	// pq 처리를 위한 Comparable 사용 : 인접노드 idx, dis 변수 설정
+	// dijkstra 연결된 노드 중 낮은 번호부터 뽑아내기 위해 정렬 > PQ
 	static class Node implements Comparable<Node> {
-		int idx, dis;
+		int f, t, c;
 
-		public Node(int idx, int dis) {
-			this.idx = idx;
-			this.dis = dis;
+		public Node(int f, int t, int c) {
+			this.f = f;
+			this.t = t;
+			this.c = c;
 		}
 
 		@Override
 		public int compareTo(Node other) {
-			if (this.dis < other.dis) {
+			if (this.c < other.c) {
 				return -1;
 			}
 			return 1;
 		}
 	}
+	// MAX값 = 최대간선 수(10000) * 최대 거리(100) + 1
+	static final int INF = 1000001;
 
-	static final int INF = 1000001;	// 최대값 : N * M + 1
-	static int N, M, X;
-	static int to_D[], from_D[];
-	static ArrayList<ArrayList<Node>> to_Graph = new ArrayList<ArrayList<Node>>();
-	static ArrayList<ArrayList<Node>> from_Graph = new ArrayList<ArrayList<Node>>();
-	static int ans;
+	static int D_to[], D_from[];
+	static ArrayList<ArrayList<Node>> gra_to, gra_from;
+	static int N, M, X, ans;
 
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/G01_dijkstra/G01_ex03_Adv_100_1238_파티.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		/*
+		 * X 입장에서 생각해볼 것 
+		 * 1. 그래프 단방향 2개 생성 
+		 * 2. 거리 배열도 2개 생성 
+		 * 3. 거리 배열에 INF 넣기 
+		 * 4. dijkstra * 메소드 작성 = PQ 사용한 향상된 dijkstra 
+		 * 5. d[] max 값 찾아서 답 넣기
+		 */
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		X = Integer.parseInt(st.nextToken());
 		
-		// 거리 배열 선언 및 INF 처리
-		to_D = new int[N + 1];
-		from_D = new int[N + 1];
-		Arrays.fill(to_D, INF);
-		Arrays.fill(from_D, INF);
-		
-		// List 생성
-		for (int i = 0; i <= N; i++) {
-			to_Graph.add(new ArrayList<Node>());
-			from_Graph.add(new ArrayList<Node>());
-		}		
+		// 거리 선언 후 INF 초기화
+		D_to = new int[N + 1];
+		D_from = new int[N + 1];
 
-		// 인접 List 입력
+		Arrays.fill(D_to, INF);
+		Arrays.fill(D_from, INF);
+
+		gra_to = new ArrayList<ArrayList<Node>>();
+		gra_from = new ArrayList<ArrayList<Node>>();
+
+		for (int i = 0; i <= N; i++) {
+			gra_to.add(new ArrayList<Node>());
+			gra_from.add(new ArrayList<Node>());
+		}
+		
 		for (int i = 1; i <= M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			int distance = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
 
-			to_Graph.get(from).add(new Node(to, distance));
-			from_Graph.get(to).add(new Node(from, distance));
+			// 정상적으로 입력 : X > 돌아가는 길 찾기
+			gra_from.get(from).add(new Node(from, to, cost));
+			// 역순으로 입력 : X > 로 오는 길 찾기
+			gra_to.get(to).add(new Node(to, from, cost));
 		}
 
-		dijkstra(X, to_Graph, to_D);
-		dijkstra(X, from_Graph, from_D);
+		diskstra(gra_from, D_from, X);
+		diskstra(gra_to, D_to, X);
 
 		ans = 0;
-
 		for (int i = 1; i <= N; i++) {
-			ans = Math.max(ans, to_D[i] + from_D[i]);
+			if (i == X) {
+				continue;
+			}
+			ans = Math.max(ans, D_to[i] + D_from[i]);
 		}
 
-		System.out.println(ans);
+		bw.write(ans + "\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
 
 	}
 
-	private static void dijkstra(int start, ArrayList<ArrayList<Node>> gra, int[] d) {
-		// pq 선언 및 시작점 입력
+	private static void diskstra(ArrayList<ArrayList<Node>> gra, int[] d, int x) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
-		// 시작점 거리 0 입력, 시작시점에 나머지는 INF가 들어가있음
-		d[start] = 0;
-		// 방문처리를 위한 check 배열 입력
-		boolean check[] = new boolean[N + 1];
+		boolean visited[];
+		visited = new boolean[N + 1];
+		// 시작지점 x 입력
+		pq.offer(new Node(0, x, 0));
+		// 시작지점 초기거리값 0 입력 > 나머지는 이미 INF 처리되어 있음
+		d[x] = 0;
 
 		while (!pq.isEmpty()) {
-			// 인접리스트 꺼내면서 확인
+			// pq기에 연결된 노드가 오름차순 순으로 나옴
 			Node now = pq.poll();
+			int now_node = now.t;
 			
-			// 방문확인 및 미방문 노드 방문처리
-			if (check[now.idx]) {
+			// 중복체크
+			if (visited[now_node]) {
 				continue;
 			}
-			check[now.idx] = true;
+			visited[now_node] = true;
 			
-			// * now.idx 노드의 인접노드 완전탐색
-			for (Node next : gra.get(now.idx)) {
-				// 인접노드간 거리가 가장 가까운 노드를 찾아 계속하여 최단거리 테이블 갱신
-				if (d[next.idx] > d[now.idx] + next.dis) {
-					d[next.idx] = d[now.idx] + next.dis;
-					pq.offer(new Node(next.idx, d[next.idx]));
+			// gra.get(now_node) 자체가 노드며 연결된 모든 노드들 = next
+			for (Node next : gra.get(now_node)) {
+				int next_node = next.t; // 다음 노드번호
+				int cost = next.c; // 다음 노드까지 거리
+				// * diskstra 최소거리 구하기 : 중요
+				if (d[next_node] > d[now_node] + cost) {
+					d[next_node] = d[now_node] + cost;
+					pq.offer(new Node(now_node, next_node, d[next_node]));
 				}
 			}
 		}
+
 	}
+
 }
