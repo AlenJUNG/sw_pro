85c0366d7300dd2197241da544fd35b901c2bdb0
SDS <SDS@DESKTOP-FJDSHCN>
2021-06-12 08:47:28 +0900

    Update

---
+++ src/G01_dijkstra/G01_ex07_Adv_P0076_효율적인도로건설_중상_hj.java
@@ -0,0 +1,13 @@
+package G01_dijkstra;
+
+import java.io.*;
+import java.util.*;
+
+public class G01_ex07_Adv_P0076_효율적인도로건설_중상_hj {
+
+	public static void main(String[] args) throws IOException{
+		System.setIn(new FileInputStream("src/G01_dijkstra/G01_ex07_Adv_P0076_효율적인도로건설_중상.txt"));
+		BufferedReader br = new BufferedReader(null)
+	}
+
+}
