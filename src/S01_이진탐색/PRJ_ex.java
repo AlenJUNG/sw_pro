d8df71c3bbcf2ab0cc1c92d3b04bfdd015f3d3fb
shadyhj <42958372+shadyhj@users.noreply.github.com>
2021-06-13 08:03:42 +0900

    Update

--- src/S01_이진탐색/PRJ_ex.java
+++ src/S01_이진탐색/PRJ_ex.java
@@ -9,12 +9,13 @@ import java.util.*;
  */
 
 public class PRJ_ex {
-	static int arr[] = {1, 4, 5, 7, 7, 10, 15, 17, 20 , 21};
-	static int findNo[] = {3, 5, 7, 17};
-	
-	public static void main(String[] args) throws IOException{
+	static int arr[] = { 1, 4, 5, 7, 7, 10, 15, 17, 20, 21 };
+	static int findNo[] = { 3, 5, 7, 17 };
 
-		for(int i = 1; i <= findNo.length; i++) {
+	public static void main(String[] args) throws IOException {
+
+		// 1부터 배열의 크기까지
+		for (int i = 1; i <= findNo.length; i++) {
 			System.out.println(binarySearch(arr, 1, 10, findNo[i - 1]));
 		}
 
@@ -25,18 +26,18 @@ public class PRJ_ex {
 		// 경우에 따라서는 필요 없을 수 있음
 		int start = s - 1;
 		int end = e - 1;
-a
+
 		while (true) {
 			// 답이 없는 경우 -1 출력
 			if (start > end) {
 				return -1;
 			}
-			
+
 			// ** mid값 계산 (소수점이하는 자동으로 절삭)
-            // int mid = (start + end) / 2;	의 경우
+			// int mid = (start + end) / 2; 의 경우
 			// start + end 값이 int 최대값 초과시 overflow 나기 때문에
 			// ** 아래 code로 사전에 방지할 것
-			
+
 			int mid = start + ((end - start) / 2);
 
 			if (a[mid] == target) {
