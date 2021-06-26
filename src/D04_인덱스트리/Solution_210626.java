4749b59b4ef1380b7950d8543a72fe472012be19
shadyhj <42958372+shadyhj@users.noreply.github.com>
2021-06-27 05:51:12 +0900

    Update

--- src/D04_인덱스트리/Solution_210626.java
+++ src/D04_인덱스트리/Solution_210626.java
@@ -6,117 +6,82 @@ import java.util.*;
 public class Solution_210626 {
 	static int TC, size, N, tree[];
 	static long ans;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/D04_¿Œµ¶Ω∫∆Æ∏Æ/D04_ex07_P0041_K_HeapΩ¥∆€¿Ã∫•∆Æ.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
+
 		TC = Integer.parseInt(br.readLine().trim());
-		
-		for(int tc = 1; tc <= TC; tc++) {
+
+		for (int tc = 1; tc <= TC; tc++) {
 			N = Integer.parseInt(br.readLine().trim());
-			
+
 			size = 1;
-			while(size < N) {
+			while (size < 100000) {
 				size *= 2;
 			}
-			
+
 			tree = new int[size * 2];
-			
+
 			int a, b;
 			ans = 0;
-			bw.write("#"+tc+" ");
-			int check = 1;
-			for(int i = 1; i <= N; i++) {
+			bw.write("#" + tc + " ");;
+			for (int i = 1; i <= N; i++) {
 				st = new StringTokenizer(br.readLine().trim());
 				a = Integer.parseInt(st.nextToken());
 				b = Integer.parseInt(st.nextToken());
-				
+
 				int id = size + b - 1;
-				
-				if(a == 1) {
-					tree[id] += 1;
-				}
-				else {
-					if(check == 1) {
-						for(int j = size - 1; j > 0; j--) {
-							tree[j] = tree[2*j] + tree[2*j + 1];
-						}
-						check -= 1;
-					}
+
+				if (a == 1) {					
+					update(id, 1);
+				} else {
 					int temp = search(b);
-					tree[temp] -= 1;
-					update(temp);
+					update(temp, -1);
 					ans = temp - size + 1;
 					bw.write(ans + " ");
 				}
 			}
-			
+
 			bw.newLine();
-			
-			
-			
+
 		}
-		
+
 		br.close();
 		bw.flush();
 		bw.close();
 
 	}
-	private static void update(int x) {
-		x /= 2;
-		while(x > 0) {
-			tree[x] -= 1;
-			x /= 2;
+
+	private static void update(int id, int x) {
+		tree[id] += x;
+		id /= 2;
+		while (id > 0) {
+			tree[id] += x;
+			id /= 2;
 		}		
 	}
-	
+
 	private static int search(int x) {
 		int node = 1;
-		
-		while(node < size) {
+
+		while (node < size) {
 			int child_node = node * 2;
-			
-			if(x > tree[child_node]) {
+
+			if (x > tree[child_node]) {
 				x = x - tree[child_node];
 				node = child_node + 1;
-			}else {
+			} else {
 				node = child_node;
 			}
 		}
-		
 		return node;
 	}
-
 }
 
 /*
-3 
-10 
-1 2 
-1 4 
-1 5 
-1 8 
-1 9 
-1 8 
-1 1 
-2 6 
-2 5 
-2 1 
-5 
-1 6 
-1 7 
-1 2 
-2 2 
-1 100 
-7 
-1 1 
-1 1 
-2 1 
-1 1 
-2 1 
-1 1 
-1 1
-*/
+ * 3 10 1 2 1 4 1 5 1 8 1 9 1 8 1 1 2 6 2 5 2 1 5 1 6 1 7 1 2 2 2 1 100 7 1 1 1
+ * 1 2 1 1 1 2 1 1 1 1 1
+ */
