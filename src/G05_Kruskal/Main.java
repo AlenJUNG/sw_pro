f31b2384283e7952f93013303aa9de5db9b994a3
SDS <SDS@HJ11-DJ-HOME>
2021-07-09 06:40:27 +0900

    Update

--- src/G05_Kruskal/Main.java
+++ src/G05_Kruskal/Main.java
@@ -4,98 +4,86 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static class Node implements Comparable<Node> {
-		int f, t, c;
-
-		public Node(int f, int t, int c) {
-			this.f = f;
-			this.t = t;
-			this.c = c;
+	static class Node implements Comparable<Node>{
+		int node, cost;
+		public Node(int node, int cost) {
+			this.node = node;
+			this.cost = cost;
 		}
-
 		@Override
 		public int compareTo(Node other) {
-			if (this.c < other.c) {
+			if(this.cost < other.cost) {
 				return -1;
 			}
 			return 1;
 		}
 	}
-
-	static int N, M, ans;
-	static int parent[];
-	static ArrayList<Node> al;
-
+	static int N, M, dis[][];
+	static long ans;
+	static ArrayList<Node> graph[];
+	static final int INF = 10000 + 1;
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
-		System.setIn(new FileInputStream("src/G05_Kruskal/G05_Kruskal.txt"));
+		System.setIn(new FileInputStream("src/G05_Kruskal/G05_ex00_100_1922_네트워크연결.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+		StringTokenizer st = null;
 
-		st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		parent = new int[N + 1];
-		al = new ArrayList<Node>();
-
-		int a, b, value;
-		for (int i = 1; i <= M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			value = Integer.parseInt(st.nextToken());
-			al.add(new Node(a, b, value));
-			al.add(new Node(b, a, value));
-		}
-
-		for (int i = 1; i <= N; i++) {
-			parent[i] = i;
-		}
-
-		Collections.sort(al);
-
-		ans = 0;
-		for (int i = 0; i < al.size(); i++) {
-			Node now = al.get(i);
-
-			if (find(now.f) == find(now.t)) {
-				continue;
-			} else {
-				union(now.f, now.t);
-				ans += now.c;
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		dis = new int[N + 1][N + 1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				dis[i][j] = dis[j][i] = INF;
 			}
 		}
 		
-		for(Node now : al) {
-			
+		graph = new ArrayList[N + 1];
+		for(int i = 0; i <= N; i++) {
+			graph[i] = new ArrayList<Node>();
 		}
 		
-		bw.write(ans+"\n");
-
-		br.close();
-		bw.flush();
-		bw.close();
-	}
-
-	private static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-
-		if (a > b) {
-			parent[a] = b;
-		} else {
-			parent[b] = a;
+		int f, t, c;
+		for(int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			f = Integer.parseInt(st.nextToken());
+			t = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			
+			graph[f].add(new Node(t, c));
+			graph[t].add(new Node(f, c));
 		}
+		
+		ans = 0;
+		prim();
 
 	}
 
-	private static int find(int x) {
-		if (x == parent[x]) {
-			return x;
+	private static void prim() {
+		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+		boolean visit[];
+		visit = new boolean[N + 1];
+		pq.offer(new Node(1, 0));
+		
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			System.out.println("1");
 		}
-		return parent[x] = find(parent[x]);
+		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
