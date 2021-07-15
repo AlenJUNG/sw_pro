49d1bb95b56e480194403abedcdce7146ba835df
shadyhj <shadyhj.@gmail.com>
2021-07-15 16:57:25 +0900

    Update

--- src/D04_인덱스트리/Solution_210715_알고리즘경진대회.java
+++ src/D04_인덱스트리/Solution_210715_알고리즘경진대회.java
@@ -12,7 +12,7 @@ import java.util.*;
 public class Solution_210715_알고리즘경진대회 {
 
 	static int TC, N, A[], G[], gcd_tree[], size;
-	static long ans;
+	static long sum_tree[], ans;
 
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/D04_인덱스트리/D04_ex3_P0084_알고리즘경진대회_중상.txt"));
@@ -45,40 +45,45 @@ public class Solution_210715_알고리즘경진대회 {
 			}
 
 			gcd_tree = new int[size * 2];
+			sum_tree = new long[size * 2];
 			
 			// gcd 구하는 트리
 			int id;
 			for (int i = 1; i <= N; i++) {
 				id = i + size - 1;
 				gcd_tree[id] = A[i];
+				sum_tree[id] = A[i];
 			}
 			
 			// 초기 전체 업데이트
 			for (int i = size - 1; i > 0; i--) {
 				gcd_tree[i] = GCD(gcd_tree[2 * i], gcd_tree[2 * i + 1]);
+				sum_tree[i] = sum_tree[2 * i] + sum_tree[2 * i + 1];
 			}
 
 //			System.out.println("check");
 
 			ans = 0;
-			int start, end, gcd;
-			for (int i = 1; i <= N; i++) {
-				start = i - G[i];
-				end = i + G[i];
-				
-				if (start < 1)
-					start = 1;
-				
-				if (end > N)
-					end = N;
+			if(N == 1) {
+				ans = 1;				
+			}else {
+				int start, end, gcd;
+				for (int i = 1; i <= N; i++) {
+					start = i - G[i];
+					end = i + G[i];
+					
+					if (start < 1)
+						start = 1;
+					
+					if (end > N)
+						end = N;
 
-				gcd = getGCD(start, end);
-
-				// 인원수를 gcd로 나누어 각 층의 팀을 합산함
-				for (int j = start; j <= end; j++) {
-					ans += A[j] / gcd;
+					gcd = getGCD(start, end);
+					ans += getSum(start, end) / gcd;					
 				}
+				
 			}
+			
 
 			bw.write("#" + tc + " " + ans + "\n");
 		}
@@ -87,6 +92,26 @@ public class Solution_210715_알고리즘경진대회 {
 		bw.flush();
 		bw.close();
 
+	}
+
+	private static long getSum(int start, int end) {
+		long res = 0;
+		int s = start + size - 1;
+		int e = end + size - 1;
+
+		while (s <= e) {
+			if (s % 2 == 1) {
+				res += sum_tree[s];
+				s++;
+			}
+			if (e % 2 == 0) {
+				res += sum_tree[e];
+				e--;
+			}
+			s /= 2;
+			e /= 2;
+		}
+		return res;
 	}
 
 	private static int getGCD(int start, int end) {
@@ -132,3 +157,4 @@ public class Solution_210715_알고리즘경진대회 {
 		}
 	}
 }
+
